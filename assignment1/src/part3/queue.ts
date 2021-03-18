33ebba4b08083892d70abb2e1cdacb1e9687a032
Sheer Maoz <sheermaoz@gmail.com>
2021-03-18 22:15:49 +0200

    finished implementing queue

--- assignment1/src/part3/queue.ts
+++ assignment1/src/part3/queue.ts
@@ -2,8 +2,13 @@ import { State, bind } from "./state";
 
 export type Queue = number[];
 
-export const enqueue = undefined;
+export const enqueue = (input : number) : State<Queue, number | undefined> => {
+    return (q : Queue) => [q.concat([input]), undefined];
+};
 
-export const dequeue = undefined;
+export const dequeue:State<Queue,number> =  (q) => {
+    return [q.slice(1), q[0]]
+};
 
-export const queueManip = undefined;
+export const queueManip:State<Queue, number|undefined> = 
+    bind(dequeue, x => bind(enqueue(2*x), () => bind(enqueue(x/3), ()=> dequeue)));
