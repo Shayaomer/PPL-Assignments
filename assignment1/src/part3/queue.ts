02c433df44f3418655962175da468355748d57a2
Sheer Maoz <sheermaoz@gmail.com>
2021-03-18 22:18:44 +0200

    changed types of return values

--- assignment1/src/part3/queue.ts
+++ assignment1/src/part3/queue.ts
@@ -2,7 +2,7 @@ import { State, bind } from "./state";
 
 export type Queue = number[];
 
-export const enqueue = (input : number) : State<Queue, number | undefined> => {
+export const enqueue = (input : number) : State<Queue, undefined> => {
     return (q : Queue) => [q.concat([input]), undefined];
 };
 
@@ -10,5 +10,5 @@ export const dequeue:State<Queue,number> =  (q) => {
     return [q.slice(1), q[0]]
 };
 
-export const queueManip:State<Queue, number|undefined> = 
+export const queueManip:State<Queue, number> = 
     bind(dequeue, x => bind(enqueue(2*x), () => bind(enqueue(x/3), ()=> dequeue)));
