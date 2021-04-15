d17dcc034c82f7e33101ecadcaf8a921c65025a2
Sheer Maoz <sheermaoz@gmail.com>
2021-04-15 11:06:38 +0300

    started working on 3b

--- assignment2/src/q3.ts
+++ assignment2/src/q3.ts
@@ -1,14 +1,26 @@
-import { /*ClassExp, ProcExp, */ Exp, Program } from "./L31-ast";
+import { ClassExp, ProcExp, Exp, Program } from "./L31-ast";
 import { Result, makeFailure } from "../shared/result";
+import { makeProcExp } from "../imp/L3-ast";
+import { first, rest } from "../shared/list";
+import { map } from "ramda";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-//export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
-
+export const class2proc = (exp: ClassExp): ProcExp =>
+{
+    const vars = exp.args;
+    const funcName = map(b => b.var, exp.bindings)
+    const funcBody = map(b => b.val, exp.bindings)
+    const body = []
+    const makeFunc = map(b =>  , exp.bindings)
+    makeProcExp(vars, [])
+    
+    
+    
+}
 /*
 Purpose: Transform L31 AST to L3 AST
 Signature: l31ToL3(l31AST)
