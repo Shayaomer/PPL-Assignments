db10e187cb155d42f1f1e81b9513447ad1ab9f4e
Shayaomer <72845603+Shayaomer@users.noreply.github.com>
2021-04-18 17:56:53 +0300

    finished class2proc

--- assignment2/src/q3.ts
+++ assignment2/src/q3.ts
@@ -1,8 +1,9 @@
-import { ClassExp, ProcExp, Exp, Program } from "./L31-ast";
+import { ClassExp, ProcExp, Exp, Program, CompoundExp, makeClassExp } from "./L31-ast";
 import { Result, makeFailure } from "../shared/result";
-import { makeProcExp } from "../imp/L3-ast";
+import { CExp, makeIfExp, makeProcExp, makeStrExp, makeVarDecl, VarDecl } from "./L31-ast";
 import { first, rest } from "../shared/list";
 import { map } from "ramda";
+import { makeNone } from "../shared/optional";
 
 /*
 Purpose: Transform ClassExp to ProcExp
@@ -12,19 +13,30 @@ Type: ClassExp => ProcExp
 export const class2proc = (exp: ClassExp): ProcExp =>
 {
     const vars = exp.args;
-    const funcName = map(b => b.var, exp.bindings)
-    const funcBody = map(b => b.val, exp.bindings)
-    const body = []
-    const makeFunc = map(b =>  , exp.bindings)
-    makeProcExp(vars, [])
-    
-    
+    const body = makeProcExp([makeVarDecl("msg")], strComparsions("msg", exp)).body
+    return makeProcExp(vars, body)
     
 }
+
+const strComparsions = (msg: String, exp: ClassExp): CExp[] =>
+{  
+    if (msg === 'first(exp.bindings).var')
+        return [first(exp.bindings).val]
+    else 
+        return strComparsions(msg, makeClassExp(exp.args,rest(exp.bindings)))
+}
+
 /*
 Purpose: Transform L31 AST to L3 AST
 Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+{
+
+return makeFailure("TODO");
+}
+   
+
+
+
